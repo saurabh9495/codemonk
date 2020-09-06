4fd3dcb2250a326c80eb0ecf9149b8fee12dddc5
saurabh9495 <saurabhkumar4829@gmail.com>
2020-09-06 19:21:28 +0530

    code pushed on 6th September 2020

--- Arrays And Strings/Monk and Inversions/Monk_and_Inversions.cpp
+++ Arrays And Strings/Monk and Inversions/Monk_and_Inversions.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
-typedef long long int lli;
+int arr[21][21];
+
 int main()
 {
-    lli testcases;
-    cin >> testcases;
-    for (lli t = 0; t < testcases; t++)
+    int c, t, n, i, j, k, cnt = 0, x, y;
+    cin >> t;
+    while (t--)
     {
-        vector<lli> vect;
-        lli noe, val = 0;
-        cin >> noe;
-        for (lli j = 0; j < noe; j++)
+        cin >> n;
+        c = 0;
+        for (i = 0; i < n; ++i)
         {
-            cin >> val;
-            vect.push_back(val);
+            for (j = 0; j < n; ++j)
+            {
+                cin >> arr[i][j];
+            }
         }
+
+        for (i = 0; i < n; ++i)
+        {
+            for (j = 0; j < n; ++j)
+            {
+                for (x = i; x < n; ++x)
+                {
+                    for (y = j; y < n; ++y)
+                    {
+                        if (arr[x][y] < arr[i][j])
+                            c++;
+                    }
+                }
+            }
+        }
+
+        cout << c << endl;
     }
+
     return 0;
 }
--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "code pushed on 5th September 2020"
+git commit -m "code pushed on 6th September 2020"
 git push origin master 
