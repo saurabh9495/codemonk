27a3fd97040972480e84ddde99335b2ebc4c718f
saurabh9495 <saurabhkumar4829@gmail.com>
2020-09-08 23:56:46 +0530

    code pushed on 7th September 2020

--- Sorting/Monk and Suffix Sort/Monk_and_Suffix_Sort.cpp
+++ Sorting/Monk and Suffix Sort/Monk_and_Suffix_Sort.cpp
@@ -5,8 +5,16 @@ using namespace std;
 typedef long long int lli;
 int main()
 {
+    int k;
     string str;
-    cin >> str;
-    vector<string> vect = [ 'aacb', 'acb', 'cb', 'b' ];
+    cin >> str >> k;
+    vector<string> vect;
+    for (lli i = 0; i < str.length(); i++)
+    {
+        string val = str.substr(i);
+        vect.push_back(val);
+    }
+    sort(vect.begin(), vect.end());
+    cout << vect[k - 1];
     return 0;
 }
